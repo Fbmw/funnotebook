84b361ee5de3ea70b4e6b0041385343e8373e641
Franklin <franklinbmw@msn.com>
2016-03-05 19:51:25 -0800

    install devise

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,14 +1,25 @@
 class NotesController < ApplicationController
-  def index
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @note = Note.all.order('created_at DESC')
   end
 
   def show
 
   end
 
-  def create
+  def new
+    @note = Note.new
+  end
 
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -16,19 +27,25 @@ class NotesController < ApplicationController
   end
 
   def update
-
+    @note = Note.find(note_params)
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'Edit'
+    end
   end
 
   def destroy
-
+    @note.destroy(note_params)
+    redirect_to notes_path
   end
 
   private
   def find_note
-
+    @note = Note.find(params[:id])
   end
 
   def note_params
-
+    params.require(:note).permit(:title, :content)
   end
 end
